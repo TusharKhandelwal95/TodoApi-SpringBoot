2eada770485d75c659479ed2a30a784e14401fef
TusharKhandelwal95 <khandelwaltushar95@gmail.com>
2025-08-21 12:41:10 +0530

    Handled status in response using ResponseEntity

--- src/main/java/com/example/TodoSpringApi/TodoController.java
+++ src/main/java/com/example/TodoSpringApi/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.TodoSpringApi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +21,14 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public List<Todo> getTodos() {
-        return todoList;
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok(todoList);
     }
 
     @PostMapping("/todos")
-    public Todo addTodo(@RequestBody Todo newTodo) {
+//    @ResponseStatus(HttpStatus.CREATED)  --> this could be used
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo newTodo) {
         todoList.add(newTodo);
-        return newTodo;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 }
